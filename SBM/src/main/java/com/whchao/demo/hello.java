b7337c9496143793aadf1b20cbfebc7473ef545f
FeiJiHQDa <feijihqda@126.com>
2018-05-28 00:27:21 +0800

    update code

--- SBM/src/main/java/com/whchao/demo/hello.java
+++ SBM/src/main/java/com/whchao/demo/hello.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/demo")
 public class hello {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
