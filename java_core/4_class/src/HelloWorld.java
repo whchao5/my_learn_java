244410406795d22186ebe37b7d10e63a191de3ab
whchao <feijihqda@126.com>
2018-05-30 18:50:30 +0800

    add java core 9

---
+++ java_core/4_class/src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        Date deadline = new Date();
+
+        System.out.println("Hello World!");
+        System.out.println(deadline.toString());
+
+
+
+    }
+
+
+}
