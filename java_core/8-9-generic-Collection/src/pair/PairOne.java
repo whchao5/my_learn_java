1840d336aaacf004952d7a1a4f9b972b335db892
FeiJiHQDa <feijihqda@126.com>
2018-06-07 00:10:38 +0800

    update PairOne

--- java_core/8-9-generic-Collection/src/pair/PairOne.java
+++ java_core/8-9-generic-Collection/src/pair/PairOne.java
@@ -5,6 +5,10 @@ public class PairOne<T> {
     private T first;
     private T second;
 
+    public PairOne(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
 
     public T getFirst() {
         return first;
