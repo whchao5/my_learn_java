794d0f54ea75de24bdff7d6d4af28981ab08fcd0
FeiJiHQDa <feijihqda@126.com>
2018-06-07 00:09:09 +0800

    add java core

---
+++ java_core/8-9-generic-Collection/src/pair/PairOne.java
@@ -0,0 +1,24 @@
+package pair;
+
+public class PairOne<T> {
+
+    private T first;
+    private T second;
+
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
---
+++ java_core/8-9-generic-Collection/src/pair/PairOneTest.java
@@ -0,0 +1,32 @@
+package pair;
+
+public class PairOneTest {
+
+    public static void main(String[] args) {
+        String[] words = {"Mary", "had", "a", "little", "lamb"};
+        PairOne<String> mm = ArrayAlg.minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
+
+ class ArrayAlg {
+
+    public static PairOne<String> minmax(String[] a) {
+
+        if (a == null || a.length == 0)
+            return null;
+        String min = a[0];
+        String max = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0)
+                min = a[i];
+            if (max.compareTo(a[i]) < 0)
+                max = a[i];
+        }
+
+        return new PairOne<>(min, max);
+    }
+
+}
