cb63cd7e319c0de63b5127e5a131a9438373491f
whchao <feijihqda@126.com>
2018-07-10 00:11:25 +0800

    upate axios

--- SBM/web/static/js/cart.js
+++ SBM/web/static/js/cart.js
@@ -1,7 +1,40 @@
+var my_log = console.log.bind(console);
+
 new Vue({
 
     el : "#app",
     data : {
-        title: "Hello Cart"
+        title: "Hello Cart",
+        productList : [],
+        totalMoney : 0
+    },
+    mounted : function(){
+        this.$nextTick(function () {
+            this.cartView();
+        })
+    },
+    methods : {
+        cartView : function () {
+            var _this=this;
+            axios.get("../static/data/cartData.json").then(function (response) {
+                // handle success
+                var res = response.data;
+                _this.productList = res.result.list;
+                _this.totalMoney = res.result.totalMoney;
+                my_log(this.productList, this.totalMoney);
+            })
+        }
     }
+
 });
+
+
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+        items: [
+            { message: 'Foo' },
+            { message: 'Bar' }
+        ]
+    }
+})
