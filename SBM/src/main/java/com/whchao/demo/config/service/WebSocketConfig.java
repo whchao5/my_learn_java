1c6295f93bece1ca423aa1c80b8f98d313e96d9e
FeiJiHQDa <feijihqda@126.com>
2019-01-10 23:34:44 +0800

    update code

--- SBM/src/main/java/com/whchao/demo/config/service/WebSocketConfig.java
+++ SBM/src/main/java/com/whchao/demo/config/service/WebSocketConfig.java
@@ -2,16 +2,14 @@ package com.whchao.demo.config.service;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.*;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
 //通过@EnableWebSocketMessageBroker 注解凯旗使用STOMP协议来传输基于代理（message broker）的消息
 //这时控制器支持使用@MessageMapping，就像使用@RequestMapping一样
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
