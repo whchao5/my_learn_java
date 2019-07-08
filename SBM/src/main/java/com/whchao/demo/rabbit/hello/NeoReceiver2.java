5c2ee65a810a5d90502a40e4c0952e26800bc697
FeiJiHQDa <feijihqda@126.com>
2019-07-08 21:52:40 +0800

    update code

--- SBM/src/main/java/com/whchao/demo/rabbit/hello/NeoReceiver2.java
+++ SBM/src/main/java/com/whchao/demo/rabbit/hello/NeoReceiver2.java
@@ -7,16 +7,16 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-//@Component
-//@RabbitListener(queues = "neo")
-//public class NeoReceiver2 {
-//
-//    private static Logger log = LoggerFactory.getLogger(NeoReceiver.class);
-//
-//    @RabbitHandler
-//    public void process(String neo) {
-////        System.out.printf("Receiver : " + neo);
-//        log.info("Receiver 2 - : " + neo);
-////        System.out.println("Receiver 2 - : " + neo);
-//    }
-//}
+@Component
+@RabbitListener(queues = "neo")
+public class NeoReceiver2 {
+
+    private static Logger log = LoggerFactory.getLogger(NeoReceiver.class);
+
+    @RabbitHandler
+    public void process(String neo) {
+//        System.out.printf("Receiver : " + neo);
+        log.info("Receiver 2 - : " + neo);
+//        System.out.println("Receiver 2 - : " + neo);
+    }
+}
--- SBM/src/test/java/com/whchao/demo/redis/RedisTest.java
+++ SBM/src/test/java/com/whchao/demo/redis/RedisTest.java
@@ -1,27 +1,52 @@
 package com.whchao.demo.redis;
 
+import com.whchao.demo.entity.Girls;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisTest {
 
-
-
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void testRedis() {
         stringRedisTemplate.opsForValue().set("aaa", "111");
 
         Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+
+
+    @Test
+    public void testObjRedis() {
+
+        Girls girls = new Girls();
+        girls.setAge(18);
+        girls.setId(2);
+        girls.setName("hali");
+
+//        stringRedisTemplate.opsForValue().set("com.whc", girls);
+
+        ValueOperations<String, Girls> operations = redisTemplate.opsForValue();
+        operations.set("com.girls", girls);
+
+
+        boolean exists = redisTemplate.hasKey("com.girls");
+
+        System.out.println(operations.get("com.girls"));
+        Assert.assertTrue(exists);
     }
 }
