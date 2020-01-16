c5fe5988d7f229d0e65c68fc232b4c50c78c013c
FeiJiHQDa <feijihqda@126.com>
2020-01-17 00:09:07 +0800

    update

---
+++ Xmall/src/main/java/com/whchao/xmall/mbg/model/UmsAdmin.java
@@ -0,0 +1,9 @@
+package com.whchao.xmall.mbg.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+public class UmsAdmin  implements Serializable {
+
+
+}
